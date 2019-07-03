6c210b7e5a4550e01d7309f1d4bbcc9afbc5877f
Movie Review Sentiment Analysis <43573998+wangsn0212@users.noreply.github.com>
2019-07-03 22:41:53 +0800

    Add files via upload

---
+++ 冒泡排序/冒泡排序.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  3 22:29:49 2019
+
+@author: PPLoveXueer
+"""
+
+def bubbleSort(arr):
+    n = len(arr)
+ 
+    # 遍历所有数组元素
+    for i in range(n):
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+ 
+arr = [64, 34, 25, 12, 22, 11, 90]
+ 
+bubbleSort(arr)
+ 
+print ("排序后的数组:")
+for i in range(len(arr)):
+    print ("%d" %arr[i]),
