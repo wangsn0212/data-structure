5ed654187b1bf8d41728f5aebade6ce1b325507f
Movie Review Sentiment Analysis <43573998+wangsn0212@users.noreply.github.com>
2019-07-22 15:43:23 +0800

    Add files via upload

---
+++ 正则表达式匹配/正则表达式匹配.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# -*- coding:utf-8 -*-
+class Solution:
+    # s, pattern都是字符串
+    def match(self, s, pattern):
+        # write code here
+        if (len(s) == 0 and len(pattern) == 0):
+            return True
+        if (len(s) > 0 and len(pattern) == 0):
+            return False
+        if (len(pattern) > 1 and pattern[1] == '*'):
+            if (len(s) > 0 and (s[0] == pattern[0] or pattern[0] == '.')):
+                return (self.match(s, pattern[2:]) or self.match(s[1:], pattern[2:]) or self.match(s[1:], pattern))
+            else:
+                return self.match(s, pattern[2:])
+        if (len(s) > 0 and (pattern[0] == '.' or pattern[0] == s[0])):
+            return self.match(s[1:], pattern[1:])
+        return False
