c988ef8e381147bdf1df0b5f454871f873d450b7
Movie Review Sentiment Analysis <43573998+wangsn0212@users.noreply.github.com>
2019-07-23 20:26:57 +0800

    Add files via upload

---
+++ 删除链表中的重复节点/删除链表中重复的结点.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+class Solution:
+    def deleteDuplication(self, pHead):
+        # write code here
+        if pHead is None or pHead.next is None:
+            return pHead
+        head1 = pHead.next
+        if head1.val != pHead.val:
+            pHead.next = self.deleteDuplication(pHead.next)
+        else:
+            while pHead.val == head1.val and head1.next is not None:
+                head1 = head1.next
+            if head1.val != pHead.val:
+                pHead = self.deleteDuplication(head1)
+            else:
+                return None
+        return pHead
